c38d1499da282623e8f40063322a3440360241cd
Iury Silva de Almeida <102688380+IuryAlmeiida@users.noreply.github.com>
2023-07-25 00:35:05 -0300

    Update resistance_calculator.h
    segunda versao do projeto

--- CalculadoraDaLeiDeOhm/resistance_calculator.h
+++ CalculadoraDaLeiDeOhm/resistance_calculator.h
@@ -1,13 +1,11 @@
 #ifndef RESISTANCE_CALCULATOR_H
 #define RESISTANCE_CALCULATOR_H
 
-// Função para calcular a resistência equivalente em série
 double calculate_series_resistance(int n, double resistors[]);
-
-// Função para calcular a resistência equivalente em paralelo
 double calculate_parallel_resistance(int n, double resistors[]);
-
-// Função para calcular a resistência equivalente em misto
 double calculate_mixed_resistance(int n, double resistors[], char types[]);
+double calculate_voltage(double current, double resistance);
+double calculate_current(double voltage, double resistance);
+double calculate_resistance(double voltage, double current);
 
 #endif // RESISTANCE_CALCULATOR_H
